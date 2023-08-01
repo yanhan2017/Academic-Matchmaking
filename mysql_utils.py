2e3000dfffeec5c64117346be1619f2916880dc6
yanhan2023 <138809472+yanhan2023@users.noreply.github.com>
2023-08-01 02:21:14 -0700

    separate execute_command and query_to_df

--- mysql_utils.py
+++ mysql_utils.py
@@ -7,8 +7,11 @@ class SQLDriver:
     def __init__(self, user='root', passwd='test_root', database='academicworld'):
         self.cnx = mysql.connector.connect(user=user, passwd=passwd, database=database)
 
-    def query_to_df(self, query_str, params):
+    def execute_command(self, command, params=None):
         cursor = self.cnx.cursor()
-        cursor.execute(query_str, params)
+        cursor.execute(command, params)
+        return cursor
+    def query_to_df(self, query_str, params=None):
+        cursor = self.execute_command(query_str, params)
         df = pd.DataFrame(cursor.fetchall(), columns=[x[0] for x in cursor.description])
         return df
