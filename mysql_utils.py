74ede198f9b9175cd051812cf8ac281a07ece35b
yanhan2023 <138809472+yanhan2023@users.noreply.github.com>
2023-08-01 02:21:43 -0700

    add functions for top publications, faculties and universities

--- mysql_utils.py
+++ mysql_utils.py
@@ -15,3 +15,56 @@ class SQLDriver:
         cursor = self.execute_command(query_str, params)
         df = pd.DataFrame(cursor.fetchall(), columns=[x[0] for x in cursor.description])
         return df
+
+    def get_top_n_publications(self, keyword, year_lower, year_upper, n=5):
+        create_view = (
+            "CREATE OR REPLACE VIEW publication_score AS "
+            "SELECT publication.id, publication.title, publication.year, SUM(score*num_citations) AS KRC "
+            "FROM publication, publication_keyword AS pk, keyword "
+            "WHERE publication.id = pk.publication_Id "
+            "AND publication.year <= %s "
+            "AND publication.year >= %s "
+            "AND keyword.id = pk.keyword_id "
+            "AND keyword.name = %s "
+            "GROUP BY publication.id ")
+        self.execute_command(create_view, [year_upper, year_lower, keyword])
+
+        query = ("SELECT title, KRC "
+                 "FROM publication_score "
+                 "ORDER BY KRC DESC "
+                 "LIMIT %s")
+
+        return self.query_to_df(query, [n])
+
+    def get_top_n_faculties(self, n=5):
+        create_view = (
+            "CREATE OR REPLACE VIEW faculty_score AS "
+            "SELECT faculty.id, faculty.name, faculty.university_id, SUM(publication_score.KRC) AS KRC "
+            "FROM publication_score, faculty, faculty_publication AS fp "
+            "WHERE faculty.id = fp.faculty_Id "
+            "AND publication_score.id = fp.publication_Id "
+            "GROUP BY faculty.id "
+            "ORDER BY SUM(publication_score.KRC) DESC ")
+        self.execute_command(create_view)
+        query = ("SELECT name, KRC "
+                 "FROM faculty_score "
+                 "ORDER BY KRC DESC "
+                 "LIMIT %s")
+        return self.query_to_df(query, [n])
+
+    def get_top_n_universities(self, n=5):
+        create_view = (
+            "CREATE OR REPLACE VIEW university_score AS "
+            "SELECT university.id, university.name, university.photo_url, SUM(faculty_score.KRC) AS KRC "
+            "FROM faculty_score, university "
+            "WHERE university.id = faculty_score.university_id "
+            "GROUP BY university.id "
+            "ORDER BY SUM(faculty_score.KRC) DESC ")
+        self.execute_command(create_view)
+
+        query = ("SELECT name, KRC "
+                 "FROM university_score "
+                 "ORDER BY KRC DESC "
+                 "LIMIT %s")
+        return self.query_to_df(query, [n])
+
